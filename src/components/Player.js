57c735a384b35b0d72e2c8e4ac758ce448cea4a0
Bartek Bartosik <b.bartek987@gmail.com>
2023-12-23 21:58:05 +0100

    Changed display of player content, and after clicking editing the player name will be cleared

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Player({
   initialName,
@@ -9,11 +9,18 @@ export default function Player({
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
+  useEffect(() => {
+    if (!isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }, [isEditing, onChangeName, playerName, symbol]);
+
   function handleEditClick() {
     setIsEditing((editing) => !editing);
 
-    if (isEditing) {
-      onChangeName(symbol, playerName);
+    // Clear default name when entering edit mode
+    if (!isEditing) {
+      setPlayerName("");
     }
   }
 
@@ -22,13 +29,11 @@ export default function Player({
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-  // let btnCaption = 'Edit';
 
   if (isEditing) {
     editablePlayerName = (
       <input type="text" required value={playerName} onChange={handleChange} />
     );
-    // btnCaption = 'Save';
   }
 
   return (
